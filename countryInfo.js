076efaf22efda36810b8c53e615d6722bbdf5567
AndrzejPliszka <147741539+AndrzejPliszka@users.noreply.github.com>
2024-01-11 18:31:29 +0100

    try make this work on click

--- countryInfo.js
+++ countryInfo.js
@@ -1,24 +1,31 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const apiUrl = "https://quilled-nervous-leopon.glitch.me/";
-    fetch(apiUrl, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'},
-        body: JSON.stringify({
-          tag: "Netherlands"
-        })
+const draggableElements = document.getElementsByTagName("svg");
+for(let i = 0; i < draggableElements.length; i++){
+  draggableElements[i].addEventListener('click', () => getCountryInfoFromServ(draggableElements[i]));
+}
+
+
+function getCountryInfoFromServ(clickedElement) {
+  const apiUrl = "https://quilled-nervous-leopon.glitch.me/";
+  console.log(clickedElement.className);
+  fetch(apiUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        tag: `${clickedElement.className}`
+      })
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Network response was not ok: ${response.statusText}`);
+      }
+      return response.json();
     })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`Network response was not ok: ${response.statusText}`);
-        }
-        return response.json();
-      })
-      .then(data => {
-        const resultElement = document.getElementById('historic-info');
-        resultElement.innerHTML = `<p>${JSON.stringify(data.message)}</p>`;
-      })
-      .catch(error => {
-        console.error('Fetch error:', error.message);
-      });
-});
+    .then(data => {
+      const resultElement = document.getElementById('historic-info');
+      resultElement.innerHTML = `<p>${JSON.stringify(data.message)}</p>`;
+    })
+    .catch(error => {
+      console.error('Fetch error:', error.message);
+    });
+}
