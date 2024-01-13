52959b4afcfa44ece921ae524f4c8bdec30dbb78
AndrzejPliszka <147741539+AndrzejPliszka@users.noreply.github.com>
2024-01-13 22:21:55 +0100

    make images come from serv

--- countryInfo.js
+++ countryInfo.js
@@ -27,7 +27,6 @@ function displayCountryInfo(data){
     const resultElement = document.getElementById('historic-info');
     const info = data.message;
     const graphicsUrl = "https://cdn.glitch.global/ba2d6357-bf92-45f0-aa0b-d4c301cb3d49";
-    console.log("no  nie");
     resultElement.innerHTML = `<div id="country-name">
             <h1>${info[0].name}</h1>
             <h2>${info[0].native_name}</h2>
@@ -36,11 +35,11 @@ function displayCountryInfo(data){
         <div id="national-symbols">
             <div id="flag">
                 <img src="${graphicsUrl}/${info[0].tag}_Flag.png">
-                <p>Flag (placeholder)</p>
+                <p>Flag</p>
             </div>
             <div id="coat">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/9/97/Rijkswapen_der_Nederlanden.svg">
-                <p>Coat of Arms (placeholder)</p>
+                <img src="${graphicsUrl}/${info[0].tag}_CoA.png">
+                <p>Coat of Arms</p>
             </div>
         </div>
         <hr>
@@ -48,23 +47,23 @@ function displayCountryInfo(data){
             <audio controls>
               <source type="audio/ogg" src="https://upload.wikimedia.org/wikipedia/commons/f/f5/United_States_Navy_Band_-_Het_Wilhelmus_%28tempo_corrected%29.ogg">
             </audio>
-            <p>Anthem ${info[0].anthem}</p>
+            <p>Anthem "${info[0].anthem}"  (does not come from serv)</p>
         </div>
         <hr>
         <h3>${info[0].goverment_type}</h3>
         <div id="political-system">
             <div id="leader">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/3/35/Jacob_Merkelbach%2C_Afb_010164033120.jpg"><br>
+                <img src="${graphicsUrl}/${info[0].tag}_Leader.png"><br>
                 <p>${info[0].leader_position}<br>${info[0].leader_name}</p>
                 
             </div>
             <div id="secondary-leader">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/a/ab/Pieter_Cort_van_der_Linden.jpg"><br>
+                <img src="${graphicsUrl}/${info[0].tag}_SecondLeader.png"><br>
                 <p>${info[0].second_leader_position}<br>${info[0].second_leader_name}</p>
                 
             </div>
             <div id="parliament">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/6/63/1913_Dutch_General_Election.svg"><br>
+                <img src="${graphicsUrl}/${info[0].tag}_Parliament.png"><br>
                 <p>${info[0].parlament}</p>
             </div>
         </div>
