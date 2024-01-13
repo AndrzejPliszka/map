818f452afdd2dd031c2d85976b0301a8c2d12822
AndrzejPliszka <147741539+AndrzejPliszka@users.noreply.github.com>
2024-01-13 15:39:38 +0100

    make something apart from country name display

--- countryInfo.js
+++ countryInfo.js
@@ -27,5 +27,52 @@ function displayCountryInfo(data){
     const resultElement = document.getElementById('historic-info');
     const info = data.message;
     console.log(info);
-    resultElement.innerHTML = `<h1>${JSON.stringify(info[0].name)}</h1>`;
+    resultElement.innerHTML = `<div id="country-name">
+            <h1>${info[0].name}</h1>
+            <h2>${info[0].native_name}</h2>
+        </div>
+        <hr>
+        <div id="national-symbols">
+            <div id="flag">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/2/20/Flag_of_the_Netherlands.svg">
+                <p>Flag (placeholder)</p>
+            </div>
+            <div id="coat">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/9/97/Rijkswapen_der_Nederlanden.svg">
+                <p>Coat of Arms (placeholder)</p>
+            </div>
+        </div>
+        <hr>
+        <div id="anthem">
+            <audio controls>
+              <source type="audio/ogg" src="https://upload.wikimedia.org/wikipedia/commons/f/f5/United_States_Navy_Band_-_Het_Wilhelmus_%28tempo_corrected%29.ogg">
+            </audio>
+            <p>Anthem ${info[0].anthem}</p>
+        </div>
+        <hr>
+        <h3>${info[0].goverment_type}</h3>
+        <div id="political-system">
+            <div id="leader">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/3/35/Jacob_Merkelbach%2C_Afb_010164033120.jpg"><br>
+                <p>${info[0].leader_position}<br>${info[0].leader_name}</p>
+                
+            </div>
+            <div id="secondary-leader">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/a/ab/Pieter_Cort_van_der_Linden.jpg"><br>
+                <p>${info[0].secondary_leader_position}<br>${info[0].secondary_leader_name}</p>
+                
+            </div>
+            <div id="parliament">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/6/63/1913_Dutch_General_Election.svg"><br>
+                <p>${info[0].parlament}</p>
+            </div>
+        </div>
+        <hr>
+        <div id="other-info">
+            <p>Official Language: ${info[0].official_language}</p>
+            <p>Currency: ${info[0].currency}</p>
+            <p>Capital City: ${info[0].capital}</p>
+        </div>
+        
+    </div>`;
 }
