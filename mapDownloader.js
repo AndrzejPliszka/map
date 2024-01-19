79a3aea3362d90c0c0dfdd81d31286db0b0b9376
AndrzejPliszka <147741539+AndrzejPliszka@users.noreply.github.com>
2024-01-19 23:01:26 +0100

    apply css and add svg from serv

--- mapDownloader.js
+++ mapDownloader.js
@@ -25,7 +25,13 @@ function downloadMap() {
 
 function displayMap(data){
     const resultElement = document.getElementById('svg-container');
-    resultElement.innerHTML = JSON.stringify(data);
+    for(let i = 0; i < data.tag.length; i++){
+      resultElement.insertAdjacentHTML('beforeend', data.svg_code[i]);
+      let svgElement = resultElement.lastChild();
+      svgElement.style.left = data.x_pos[i];
+      svgElement.style.top = data.y_pos[i];
+      svgElement.style.width = data.width[i];
+    }
     
     initializeMapManager()
 }
