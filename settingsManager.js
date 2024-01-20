5a36238f397482da7c57b4b64e087c95d945004b
AndrzejPliszka <147741539+AndrzejPliszka@users.noreply.github.com>
2024-01-20 16:21:45 +0100

    fix errors

--- settingsManager.js
+++ settingsManager.js
@@ -1,9 +1,10 @@
 function changeTime(time_amount, should_increase){
   const dateObject = document.getElementById("date_input");
-  let date = Date.parse(dateObject.value);
+  let date = new Date(dateObject.value);
   switch(time_amount){
       case "day":
-        date = should_increase ? date.setDate(date.getDate + 1) : date.setDate(date.getDate - 1);
+        date = should_increase ? date.setDate(date.getDate() + 1) : date.setDate(date.getDate() - 1);
+        break;
   }
   dateObject.value = dateObject.toISOString.slice(0, 10);
   downloadMap();
