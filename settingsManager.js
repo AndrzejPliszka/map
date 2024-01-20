37f9559dbf31b26ef3a5b9bb88690c8d76f1192b
AndrzejPliszka <147741539+AndrzejPliszka@users.noreply.github.com>
2024-01-20 11:56:00 +0100

    increase and decrease day with buttons

--- settingsManager.js
+++ settingsManager.js
@@ -1,3 +1,12 @@
 function changeTime(time_amount, should_increase){
+  const dateObject = document.getElementById("date_input");
+  let date = Date.parse(dateObject.value);
+  switch(time_amount){
+      case "day":
+        date = should_increase ? date.setDate(date.getDate + 1) : date.setDate(date.getDate - 1);
+  }
+  dateObject.value = dateObject.toISOString.slice(0, 10);
+  downloadMap();
+  
   console.log(time_amount, should_increase);
 }
