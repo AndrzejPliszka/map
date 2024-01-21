7cef298d6b66059b4bf5aaa14222d37cff407b0e
AndrzejPliszka <147741539+AndrzejPliszka@users.noreply.github.com>
2024-01-21 19:41:01 +0100

    fix error

--- mapDownloader.js
+++ mapDownloader.js
@@ -39,8 +39,8 @@ function displayMap(data){
     for(let i = 0; i < data.tag.length; i++){
       resultElement.insertAdjacentHTML('beforeend', data.svg_code[i]);
       let svgElement = resultElement.lastChild;
-      svgElement.style.left = `${data.x_pos[i] + offsetX}`;
-      svgElement.style.top = `${data.y_pos[i] + offsetY}`;
+      svgElement.style.left = `${data.x_pos[i] + 0}px`;
+      svgElement.style.top = `${data.y_pos[i] + 0}px`;
       console.log(data.width[i] * zoomLevel, zoomLevel);
       svgElement.style.width = data.width[i];
     }
