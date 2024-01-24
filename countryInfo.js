d6dbb63c16bc7eada11ed2f2a49fa3d50e8a82f8
AndrzejPliszka <147741539+AndrzejPliszka@users.noreply.github.com>
2024-01-24 17:07:59 +0100

    delete index and fetch date to serv

--- countryInfo.js
+++ countryInfo.js
@@ -6,7 +6,8 @@ function getCountryInfoFromServ(clickedElement) {
       headers: {
         'Content-Type': 'application/json'},
       body: JSON.stringify({
-        tag: `${clickedElement.className.baseVal}`
+        tag: `${clickedElement.className.baseVal}`,
+        date: `${document.getElementById("date_input").value}`
       })
   })
     .then(response => {
@@ -28,17 +29,17 @@ function displayCountryInfo(data){
     const info = data.message;
     const graphicsUrl = "https://cdn.glitch.global/ba2d6357-bf92-45f0-aa0b-d4c301cb3d49";
     resultElement.innerHTML = `<div id="country-name">
-            <h1>${info[0].name}</h1>
-            <h2>${info[0].native_name}</h2>
+            <h1>${info.name}</h1>
+            <h2>${info.native_name}</h2>
         </div>
         <hr>
         <div id="national-symbols">
             <div id="flag">
-                <img src="${graphicsUrl}/${info[0].tag}_Flag.png">
+                <img src="${graphicsUrl}/${info.tag}_Flag.png">
                 <p>Flag</p>
             </div>
             <div id="coat">
-                <img src="${graphicsUrl}/${info[0].tag}_CoA.png">
+                <img src="${graphicsUrl}/${info.tag}_CoA.png">
                 <p>Coat of Arms</p>
             </div>
         </div>
@@ -47,31 +48,31 @@ function displayCountryInfo(data){
             <audio controls>
               <source type="audio/ogg" src="https://upload.wikimedia.org/wikipedia/commons/f/f5/United_States_Navy_Band_-_Het_Wilhelmus_%28tempo_corrected%29.ogg">
             </audio>
-            <p>Anthem "${info[0].anthem}"  (does not come from serv)</p>
+            <p>Anthem "${info.anthem}"  (does not come from serv)</p>
         </div>
         <hr>
         <h3>${info[0].goverment_type}</h3>
         <div id="political-system">
             <div id="leader">
-                <img src="${graphicsUrl}/${info[0].tag}_Leader.png"><br>
-                <p>${info[0].leader_position}<br>${info[0].leader_name}</p>
+                <img src="${graphicsUrl}/${info.tag}_Leader.png"><br>
+                <p>${info[0].leader_position}<br>${info.leader_name}</p>
                 
             </div>
             <div id="secondary-leader">
-                <img src="${graphicsUrl}/${info[0].tag}_SecondLeader.png"><br>
-                <p>${info[0].second_leader_position}<br>${info[0].second_leader_name}</p>
+                <img src="${graphicsUrl}/${info.tag}_SecondLeader.png"><br>
+                <p>${info[0].second_leader_position}<br>${info.second_leader_name}</p>
                 
             </div>
             <div id="parliament">
-                <img src="${graphicsUrl}/${info[0].tag}_Parliament.png"><br>
+                <img src="${graphicsUrl}/${info.tag}_Parliament.png"><br>
                 <p>${info[0].parlament}</p>
             </div>
         </div>
         <hr>
         <div id="other-info">
-            <p>Official Language: ${info[0].official_language}</p>
-            <p>Currency: ${info[0].currency}</p>
-            <p>Capital City: ${info[0].capital}</p>
+            <p>Official Language: ${info.official_language}</p>
+            <p>Currency: ${info.currency}</p>
+            <p>Capital City: ${info.capital}</p>
         </div>
         
     </div>`;
